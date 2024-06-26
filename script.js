3ce6820d41f43f18665cfe27da54fb1866b947cc
Brijesh kachadiya <128162535+birju22112002@users.noreply.github.com>
2024-06-26 20:17:58 +0530

    Update script.js
    commit

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ emailField.focus({
   preventScroll: true,
 });
 
-document.getElementById('mobile-menu-button').addEventListener('click', function() {
-    document.querySelector('.header-list').classList.toggle('active');
+document
+  .getElementById("mobile-menu-button")
+  .addEventListener("click", function () {
+    const headerList = document.querySelector(".header-list");
+    headerList.style.display =
+      headerList.style.display === "flex" ? "none" : "flex";
   });
